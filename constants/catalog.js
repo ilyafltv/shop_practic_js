52fe79e8c682d44902850ddd15572ae492a95113
ilya <keebro30@gmail.com>
2023-11-17 23:26:34 +0300

    finished project

---
+++ constants/catalog.js
@@ -0,0 +1,44 @@
+const CATALOG = [
+    {
+        id: "el1",
+        name: "Apple",
+        img: "img/catalog/apple.jpg",
+        price: 20
+    },
+    {
+        id: "el2",
+        name: "Orange",
+        img: "img/catalog/orange.jpg",
+        price: 20
+    },
+    {
+        id: "el3",
+        name: "Tomato",
+        img: "img/catalog/tomato.jpg",
+        price: 30
+    },
+    {
+        id: "el4",
+        name: "Picle",
+        img: "img/catalog/picle.jpg",
+        price: 30
+    },
+    {
+        id: "el5",
+        name: "Carrot",
+        img: "img/catalog/carrot.jpg",
+        price: 40
+    },
+    {
+        id: "el6",
+        name: "Watermelon",
+        img: "img/catalog/watermelon.jpg",
+        price: 120
+    },
+    {
+        id: "el7",
+        name: "Melon",
+        img: "img/catalog/melon.jpg",
+        price: 90
+    }
+];
